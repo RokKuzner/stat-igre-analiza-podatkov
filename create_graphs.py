7b6f97a0f88aecc019329d2df0998c4e349b4055
Rok <rok.ankaran@gmail.com>
2026-02-09 19:14:03 +0100

    added primerjava prosti cas and calcualtion of "razpon"

--- create_graphs.py
+++ create_graphs.py
@@ -116,6 +116,7 @@ def primerjava_zdravje(save_location = 'data/graphs'):
     datapoint_map.sort(key=lambda x: x[1])
     x_values = [point[1] for point in datapoint_map]
     y_values = [point[0] for point in datapoint_map]
+    print(max(y_values), min(y_values), max(y_values)-min(y_values), (max(y_values)-min(y_values))/5)
 
     fig, ax = plot(x_values, y_values, "neto dohodek na prebivalca v €", "ocena zdravja (1-5)", f"Korelacija: neto dohodek na prebivalca proti ocena zdravja")
 
@@ -155,6 +156,31 @@ def zdravje_po_regijah(save_location = 'data/graphs'):
 
     plt.close(fig)
 
+def primerjava_prosti_cas(save_location = 'data/graphs'):
+    datapoint_map = []
+
+    for i in range(1, 13):
+        prosti_cas_score = float(database.get_stat_info("osebe", 2022, i, "Ocena_čas"))
+        comparator_stat = database.get_stat_info("osebe", 2022, i, "Neto_preb")
+
+        datapoint_map.append([
+                prosti_cas_score, 
+                comparator_stat
+            ])
+        
+    datapoint_map.sort(key=lambda x: x[1])
+    x_values = [point[1] for point in datapoint_map]
+    y_values = [point[0] for point in datapoint_map]
+
+    print(max(y_values), min(y_values), max(y_values)-min(y_values), (max(y_values)-min(y_values))/10)
+
+    fig, ax = plot(x_values, y_values, "neto dohodek na prebivalca v €", "ocena prostega čas (1-10)", "Korelacija: neto dohodek na prebivalca proti ocena prostega časa")
+
+    fig.savefig(f"{save_location}/denar_proti_prosti_cas.png")
+
+    plt.close(fig)
+
 if __name__ == "__main__":
-    primerjava_zdravje()
-    zdravje_po_regijah()
+    #primerjava_zdravje()
+    #zdravje_po_regijah()
+    primerjava_prosti_cas()
